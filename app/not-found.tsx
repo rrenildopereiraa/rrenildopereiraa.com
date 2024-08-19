852b1ef9d6b9d78f613cf847b97ef7df37cc9220
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-08-19 18:24:43 +0100

    update introducing-yummacss.mdx and not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,7 +2,10 @@ export default function NotFound() {
   return (
     <section>
       <h1 className="mb-8 fs-xl fw-600 ls-xs">404</h1>
-      <p className="mb-4">This page could not be found.</p>
+      <p className="mb-4">
+        Oh, I'm so sorry! It looks like the page you were looking for has taken
+        a vacation. Why not come back next Monday and have another look?
+      </p>
     </section>
   );
 }
