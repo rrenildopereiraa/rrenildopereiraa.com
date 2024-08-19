8f2cd00fa7fe8419ab6de7aa0f0802c8fbc53dcb
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-08-19 18:13:55 +0100

    update footer.tsx and add icons.tsx

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,19 +1,5 @@
-function ArrowIcon() {
-  return (
-    <svg
-      width="12"
-      height="12"
-      viewBox="0 0 12 12"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
-        fill="currentColor"
-      />
-    </svg>
-  );
-}
+import React from "react";
+import { GitHubIcon, TwitterIcon, RssIcon } from "./icons";
 
 export default function Footer() {
   return (
@@ -24,9 +10,9 @@ export default function Footer() {
             className="d-f ai-c h:tc-black"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://x.com/rrenildoo"
+            href="https://twitter.com/rrenildoo"
           >
-            <ArrowIcon />
+            <TwitterIcon className="dim-5" />
             <p className="ml-2 h-7">Twitter</p>
           </a>
         </li>
@@ -37,7 +23,7 @@ export default function Footer() {
             target="_blank"
             href="https://github.com/rrenildopereiraa"
           >
-            <ArrowIcon />
+            <GitHubIcon className="dim-5" />
             <p className="ml-2 h-7">GitHub</p>
           </a>
         </li>
@@ -48,14 +34,12 @@ export default function Footer() {
             target="_blank"
             href="/rss"
           >
-            <ArrowIcon />
+            <RssIcon className="dim-5" />
             <p className="ml-2 h-7">RSS</p>
           </a>
         </li>
       </ul>
-      <p className="mt-8 tc-lead">
-        © {new Date().getFullYear()} MIT Licensed
-      </p>
+      <p className="mt-8 tc-lead">© {new Date().getFullYear()} MIT Licensed</p>
     </footer>
   );
 }
---
+++ app/components/icons.tsx
@@ -0,0 +1,121 @@
+import React from "react";
+
+export function LinkIcon({ className }: { className?: string }) {
+  return (
+    <svg
+      width="12px"
+      height="12px"
+      strokeWidth="1.5"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      color="currentColor"
+      className={className}
+    >
+      <path
+        d="M14 11.9976C14 9.5059 11.683 7 8.85714 7C8.52241 7 7.41904 7.00001 7.14286 7.00001C4.30254 7.00001 2 9.23752 2 11.9976C2 14.376 3.70973 16.3664 6 16.8714C6.36756 16.9525 6.75006 16.9952 7.14286 16.9952"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M10 11.9976C10 14.4893 12.317 16.9952 15.1429 16.9952C15.4776 16.9952 16.581 16.9952 16.8571 16.9952C19.6975 16.9952 22 14.7577 22 11.9976C22 9.6192 20.2903 7.62884 18 7.12383C17.6324 7.04278 17.2499 6.99999 16.8571 6.99999"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+export function TwitterIcon({ className }: { className?: string }) {
+  return (
+    <svg
+      width="12px"
+      height="12px"
+      strokeWidth="1.5"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      color="currentColor"
+      className={className}
+    >
+      <path
+        d="M23 3.01006C23 3.01006 20.9821 4.20217 19.86 4.54006C19.2577 3.84757 18.4573 3.35675 17.567 3.13398C16.6767 2.91122 15.7395 2.96725 14.8821 3.29451C14.0247 3.62177 13.2884 4.20446 12.773 4.96377C12.2575 5.72309 11.9877 6.62239 12 7.54006V8.54006C10.2426 8.58562 8.50127 8.19587 6.93101 7.4055C5.36074 6.61513 4.01032 5.44869 3 4.01006C3 4.01006 -1 13.0101 8 17.0101C5.94053 18.408 3.48716 19.109 1 19.0101C10 24.0101 21 19.0101 21 7.51006C20.9991 7.23151 20.9723 6.95365 20.92 6.68006C21.9406 5.67355 23 3.01006 23 3.01006Z"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      ></path>
+    </svg>
+  );
+}
+
+export function GitHubIcon({ className }: { className?: string }) {
+  return (
+    <svg
+      width="12px"
+      height="12px"
+      strokeWidth="1.5"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      color="currentColor"
+      className={className}
+    >
+      <path
+        d="M16 22.0268V19.1568C16.0375 18.68 15.9731 18.2006 15.811 17.7506C15.6489 17.3006 15.3929 16.8902 15.06 16.5468C18.2 16.1968 21.5 15.0068 21.5 9.54679C21.4997 8.15062 20.9627 6.80799 20 5.79679C20.4558 4.5753 20.4236 3.22514 19.91 2.02679C19.91 2.02679 18.73 1.67679 16 3.50679C13.708 2.88561 11.292 2.88561 8.99999 3.50679C6.26999 1.67679 5.08999 2.02679 5.08999 2.02679C4.57636 3.22514 4.54413 4.5753 4.99999 5.79679C4.03011 6.81549 3.49251 8.17026 3.49999 9.57679C3.49999 14.9968 6.79998 16.1868 9.93998 16.5768C9.61098 16.9168 9.35725 17.3222 9.19529 17.7667C9.03334 18.2112 8.96679 18.6849 8.99999 19.1568V22.0268"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      ></path>
+      <path
+        d="M9 20.0267C6 20.9999 3.5 20.0267 2 17.0267"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      ></path>
+    </svg>
+  );
+}
+
+export function RssIcon({ className }: { className?: string }) {
+  return (
+    <svg
+      width="12px"
+      height="12px"
+      strokeWidth="1.5"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      color="currentColor"
+      className={className}
+    >
+      <path
+        d="M12 19C12 14.8 9.2 12 5 12"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M19 19C19 10.6 13.4 5 5 5"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M5 19.01L5.01 18.9989"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
