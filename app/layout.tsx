d33cefd1cb408fbc91d60d37c28d4b1cc586b8aa
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-11-01 15:48:14 +0000

    app [style]:
    - remove fonts in `layout.tsx` page
    - update `not-found.tsx` page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,10 @@
 import "./global.css";
-import type { Metadata } from "next";
-import { GeistSans } from "geist/font/sans";
-import { GeistMono } from "geist/font/mono";
-import { Navbar } from "./components/nav";
 import { Analytics } from "@vercel/analytics/react";
+import { baseUrl } from "./sitemap";
+import { Navbar } from "./components/nav";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import Footer from "./components/footer";
-import { baseUrl } from "./sitemap";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
@@ -14,16 +12,14 @@ export const metadata: Metadata = {
     default: "Renildo Pereira | DX at Yumma CSS",
     template: "%s | Renildo Pereira | DX at Yumma CSS",
   },
-  description:
-    "Creator of Yumma CSS. Freelancer maintaining and developing Yumma CSS projects.",
+  description: "Creator of Yumma CSS. Freelancer maintaining and developing Yumma CSS projects.",
   icons: {
     icon: "https://rrenildopereiraa.vercel.app/favicon.ico",
     apple: "https://rrenildopereiraa.vercel.app/apple-icon.png",
   },
   openGraph: {
     title: "Renildo Pereira | DX at Yumma CSS",
-    description:
-      "Creator of Yumma CSS. Freelancer maintaining and developing Yumma CSS projects.",
+    description: "Creator of Yumma CSS. Freelancer maintaining and developing Yumma CSS projects.",
     url: baseUrl,
     siteName: "Renildo Pereira | DX at Yumma CSS",
     locale: "en_US",
@@ -49,20 +45,9 @@ export const metadata: Metadata = {
 
 const cx = (...classes) => classes.filter(Boolean).join(" ");
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html
-      lang="en"
-      className={cx(
-        "tc-black bg-white",
-        GeistSans.variable,
-        GeistMono.variable
-      )}
-    >
+    <html lang="en" className={cx("tc-black bg-white")}>
       <body className="max-w-144 mx-4 mt-8 lg:mx-auto">
         <main className="f-auto min-w-0 mt-6 d-f fd-c px-2 md:px-0">
           <Navbar />
