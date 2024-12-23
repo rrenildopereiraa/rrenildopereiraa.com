bb73e97db854dbe6754338bbf02d5dac9f2b2e33
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-12-23 01:30:36 +0000

    update metaData with new name and description

--- app/config.ts
+++ app/config.ts
@@ -1,15 +1,13 @@
 export const metaData = {
   baseUrl: "https://Renildo-template.vercel.app/",
   title: "Renildo",
-  name: "Sirius",
+  name: "Renildo",
   ogImage: "/og.png",
-  description: "A clean, fast, and lightweight portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
+  description: "Creator of Yumma CSS. Front-end developer and designer. I love to create beautiful and functional websites.",
 };
 
 export const socialLinks = {
   twitter: "https://x.com/rrenildoo",
   github: "https://github.com/rrenildopereiraa",
-  instagram: "https://www.instagram.com/rrenildopereiraa",
   linkedin: "https://www.linkedin.com/in/rrenildopereiraa/",
-  email: "mailto:rrenildopereiraa@gmail.com",
 };
