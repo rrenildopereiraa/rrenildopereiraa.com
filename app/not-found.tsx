1fb094a90d19952383e180f971e4936b3bcc3a99
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-12-23 14:37:15 +0000

    increase font size for 404 error message in NotFound component

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
 export default function NotFound() {
   return (
     <section>
-      <h1 className="fw-500 fs-sm mb-8 ls-2">404 - Page not found</h1>
+      <h1 className="fw-500 fs-xxl mb-8 ls-2">404 - Page not found</h1>
       <p className="mb-4">Oops! The page you're looking for doesn't seem to exist.</p>
     </section>
   );
